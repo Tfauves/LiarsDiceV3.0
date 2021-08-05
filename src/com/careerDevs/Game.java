f1aead1b51a98625a49f5648eff34995b8f19c5c
Tfauves <tfauvelle@gmail.com>
2021-08-05 14:51:43 -0400

    Initialized guess method, lie check and bid validate.

--- src/com/careerDevs/Game.java
+++ src/com/careerDevs/Game.java
@@ -9,7 +9,11 @@ public class Game {
     private final int MAX_PLAYERS = 6;
     private final int MIN_PLAYERS = 1;
     public int initialBidDiceQty;
-    public int initialBidDiceFaceValue
+    public int initialBidDiceFaceValue;
+    public int nextGuessDiceQty;
+    public int nextGuessDiceFaceValue;
+    public boolean isALie = false;
+    public boolean isActiveRound = false;
 
     public Game() {
         System.out.println("Enter amount of players: ");
@@ -46,15 +50,71 @@ public class Game {
 
     }
 
-
-
     public void makeBid() {
         System.out.println("make your bid: ");
         System.out.println("Enter qty of dice on the table: ");
         initialBidDiceQty = scanner.nextInt();
         System.out.println("Enter dice face value: ");
         initialBidDiceFaceValue = scanner.nextInt();
-        System.out.println();
+        scanner.nextLine();
+
+
+    }
+
+    public void guessOrCall() {
+        System.out.println("Type (bid) to bid or (lie) if you think the bid is a lie.");
+        String playerGuess = scanner.nextLine();
+        if (playerGuess.equals("bid")) {
+            System.out.println("Enter qty of dice on table: ");
+            nextGuessDiceQty = scanner.nextInt();
+            System.out.println("Enter face value: ");
+            nextGuessDiceFaceValue = scanner.nextInt();
+//            betRecordDisplay = player.playerName + " 's bid is: " + secondBidHowManyDice + "x " + secondBidDiceFaceValue;
+            validateBid();
+
+
+        } else if (playerGuess.equals("lie")) {
+            checkLie();
+        }
+    }
+
+    public void validateBid() {
+        if (nextGuessDiceQty > initialBidDiceQty) {
+            System.out.println("Valid bid");
+
+
+        } else if (nextGuessDiceQty == initialBidDiceQty
+                && nextGuessDiceFaceValue > initialBidDiceFaceValue) {
+            System.out.println("Valid bid");
+
+
+        } else {
+            System.out.println("Invalid bid, bid again");
+
+        }
+    }
+
+    public void checkLie() {
+        if (diceOnTable.containsKey(initialBidDiceFaceValue) && diceOnTable.get(initialBidDiceFaceValue) >= initialBidDiceQty) {
+            System.out.println("bid was true challenger loses");
+            isALie = false;
+            isActiveRound = false;
+            return;
+
+        } else {
+            isALie = true;
+            isActiveRound = false;
+        }
+        if (isALie) {
+            System.out.println("bid was a lie");
+            System.out.println(player.playerName + " loses a die.");
+            player.cup.dice.remove(0);
+
+            if (player.cup.dice.size() == 0) {
+                System.out.println(player.playerName + " is out of dice. You are out of the game");
+            }
+        }
+    }
 
     }
 
